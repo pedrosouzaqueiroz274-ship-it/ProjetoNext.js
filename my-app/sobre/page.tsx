4602480b9fbce9fc8cbb7dd716382c9a425fd650
pedrosouzaqueiroz <pedrosouzaqueiroz274@gmail.com>
2026-05-29 08:47:24 -0300

    Atualiza descrição pessoal

--- my-app/sobre/page.tsx
+++ my-app/sobre/page.tsx
@@ -3,8 +3,8 @@ import Titulo from "../Titulo";
 export default function Sobre() {
   return (
     <main>
-      <Titulo texto="Pedro Souza Queiroz" />
-      <p>Breve descrição sobre mim aqui.</p>
+      <Titulo texto="Pedro Queiroz de Souza" />
+      <p>Bom..., meu nome é Pedro estou no segundo ano do ensino médio e tecnico em informatica na escola ETPC, tenho uma irmao que se formou na etpc e agora faz faculdade , sou grato por tudo q tenho e tinha a chance de ter.  .</p>
     </main>
   );
 }
