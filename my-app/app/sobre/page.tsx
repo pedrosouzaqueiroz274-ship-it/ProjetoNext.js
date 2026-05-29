59adc39d590621a8821329cc74055d6d09f5fce2
pedrosouzaqueiroz <pedrosouzaqueiroz274@gmail.com>
2026-05-29 09:07:14 -0300

    Ajusta espaçamento da página sobre

--- my-app/app/sobre/page.tsx
+++ my-app/app/sobre/page.tsx
@@ -5,7 +5,7 @@ export default function Sobre() {
     <main style={{ padding: "20px" }}>
       <Titulo texto="Pedro Queiroz de Souza e Davi Lopes Marques" />
       <br />
-      <p>Bom..., meu nome é Pedro estou no segundo ano do ensino médio e tecnico em informatica na escola ETPC, tenho uma irmao que se formou na etpc e agora faz faculdade , sou grato por tudo q tenho e tinha a chance de ter. Fiz essa parte prática com meu amigo Davi Lopes</p>
+      <p>Bom..., meu nome é Pedro estou no segundo ano do ensino médio e tecnico em informatica na escola ETPC, tenho uma irmao que se formou na etpc e agora faz faculdade. Fiz essa parte prática com meu amigo Davi Lopes :)</p>
     </main>
   );
 }
